beef35eac79eeb2d2a54122f3463a5597479776b
JangHoonSeok <orirori512@naver.com>
2023-05-10 15:49:10 +0900

    🔧 chore: eslint 규칙 추가
    props 타입 지정 끄기
    return 하고 괄호 없이 반환 끄기
    사용하지 않는 변수 warning
    하나만 export할때 default로 해야만하는 규칙 off

--- FE/.eslintrc.js
+++ FE/.eslintrc.js
@@ -18,5 +18,12 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'react/no-unescaped-entities': 0, //
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/prop-types': 0, // prop의 타입 지정 끄기.
+    'arrow-body-style': 0, // return 하고 괄호 없이 반환 끄기
+    'no-unused-vars': 1, // 사용하지 않는 변수 warning
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0, // 하나만 export할때 default로 해야만하는 규칙 off
   },
 };
