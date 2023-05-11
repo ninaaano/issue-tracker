04cf5689a545b34a1ae0b0e4c651dc96d5426666
wnsqhs <wnsqhs7804@gmail.com>
2023-05-12 01:10:32 +0900

    feat: TabBarController를 rootViewController로 설정
    - Main Stroyboard 삭제로 인해 앱 실행 시 tabBarViewController가 rootViewController가 되도록 설정

--- iOS/issue-tracker/issue-tracker/SceneDelegate.swift
+++ iOS/issue-tracker/issue-tracker/SceneDelegate.swift
@@ -14,8 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let tabBarViewController = TabBarViewController()
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = tabBarViewController
         window.makeKeyAndVisible()
         
         self.window = window
