73eb2277c045dd68c268c43bf05a54c4fc6b1981
YouSooA <kinsk2839@naver.com>
2023-05-12 16:27:20 +0900

    feat: Header 컴포넌트의 user 이미지 style 적용

--- FE/src/components/Header/style.js
+++ FE/src/components/Header/style.js
@@ -4,6 +4,12 @@ const $Header = styled.header`
   display: flex;
   justify-content: space-between;
   padding: 27px 0;
+
+  & > img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+  }
 `;
 
 export { $Header };
