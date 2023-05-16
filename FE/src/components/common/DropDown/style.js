0eda24f8ab89347b7b8332181f1240048ec8cae3
JangHoonSeok <orirori512@naver.com>
2023-05-16 16:46:18 +0900

    ✨ feat: 드롭다운 컴포넌트 완료

--- FE/src/components/common/DropDown/style.js
+++ FE/src/components/common/DropDown/style.js
@@ -11,7 +11,12 @@ const $DropDownHeader = styled.header`
   width: 100%;
   height: 36px;
   padding: 8px 16px;
-  background-color: ${({ theme }) => theme.colors.neutral.background.default};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  border-radius: 16px 16px 0px 0px;
+  background-color: ${({ theme }) => theme.colors.neutral.background.bold};
   color: ${({ theme }) => theme.colors.neutral.text.strong};
   font-weight: ${({ theme }) => theme.fontWeight.regular};
   font-size: ${({ theme }) => theme.fontSize.S.fontSize};
