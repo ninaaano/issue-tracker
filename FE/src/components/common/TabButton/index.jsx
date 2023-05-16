b23db1ef8e43f6bc388d6cdc6a84ee73a9eef149
YouSooA <kinsk2839@naver.com>
2023-05-16 23:04:39 +0900

    refactor: Button 컴포넌트를 이용해 TabButton 구현

--- FE/src/components/common/TabButton/index.jsx
+++ FE/src/components/common/TabButton/index.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Icon from '../Icon';
-import { $TabButtonWrapper, $TabRightButton, $TabLeftButton } from './style';
+import { $TabButton, $LeftButton, $RightButton } from './style';
 
 const TabButton = ({ labelCount = 0, milestoneCount = 0 }) => {
-  // TODO: 탭버튼 상태 관리해야함.
   return (
-    <$TabButtonWrapper>
-      <$TabRightButton>
+    <$TabButton>
+      <$LeftButton type="ghost" size="M">
         <Icon name="label" />
         {`레이블(${labelCount})`}
-      </$TabRightButton>
-      <$TabLeftButton>
-        <Icon name="milestone" />
+      </$LeftButton>
+      <$RightButton type="ghost" size="M">
+        <Icon name="label" />
         {`마일스톤(${milestoneCount})`}
-      </$TabLeftButton>
-    </$TabButtonWrapper>
+      </$RightButton>
+    </$TabButton>
   );
 };
 
