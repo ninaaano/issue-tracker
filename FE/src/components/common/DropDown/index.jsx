52cd8c7568ca5116b09575b4f8a360aed094d818
JangHoonSeok <orirori512@naver.com>
2023-05-16 15:29:23 +0900

    ✨ feat: 드롭 다운 styled component 헤더까지 완료

--- FE/src/components/common/DropDown/index.jsx
+++ FE/src/components/common/DropDown/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Icon from '../Icon';
+import { $DropDownHeader, $DropDownMenus, $DropDownWrapper } from './style';
+import DropDownMenu from './DropDownMenu';
 
-const DropDown = (props) => {
-  return <div>DropDown</div>;
+const DropDown = ({ modalName, menus }) => {
+  return (
+    <$DropDownWrapper>
+      <$DropDownHeader>{modalName}</$DropDownHeader>
+      <$DropDownMenus>
+        {menus.map((menu) => (
+          /*
+            menu : {
+                id: map으로 돌리기 위한 id 설정.
+                imgSrc: 이미지 주소 or 어떻게 할지 생각.
+                text: 메뉴 텍스트 ex) 내가 작성한 이슈
+            }
+          */
+          <DropDownMenu key={menu.id} menuImg={menu.imgSrc} menuText={menu.text} />
+        ))}
+      </$DropDownMenus>
+    </$DropDownWrapper>
+  );
 };
 
-DropDown.propTypes = {};
+DropDown.propTypes = {
+  modalName: PropTypes.string.isRequired,
+  menus: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default DropDown;
--- FE/src/components/common/DropDown/style.js
+++ FE/src/components/common/DropDown/style.js
@@ -1,9 +1,26 @@
 import styled from 'styled-components';
 
-const $DropDownWrapper = styled.section``;
+const $DropDownWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 240px;
+`;
 
-const $DropDownHeader = styled.header``;
+const $DropDownHeader = styled.header`
+  width: 100%;
+  height: 36px;
+  padding: 8px 16px;
+  background-color: ${({ theme }) => theme.colors.neutral.background.default};
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  font-size: ${({ theme }) => theme.fontSize.S.fontSize};
+`;
 
-const $DropDownMenus = styled.ul``;
+const $DropDownMenus = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
 export { $DropDownWrapper, $DropDownHeader, $DropDownMenus };
