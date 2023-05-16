6f8662ed9aa9a61b80f7f4671211d28231b8c2f9
JangHoonSeok <orirori512@naver.com>
2023-05-17 00:14:32 +0900

    ✨ feat: 이슈 리스트 메인 뷰 - 이슈하나의 목록 뷰 완성

--- FE/src/components/IssueList/IssueListMain/List/index.jsx
+++ FE/src/components/IssueList/IssueListMain/List/index.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const List = (props) => {
-  return <div>List</div>;
+import Icon from '../../../common/Icon';
+import Label from '../../../common/Label';
+import { $List, $LeftElements, $UpElements, $IssueTitle, $IssueInfo, $Assignee } from './style';
+
+const List = ({ issueId, issueTitle, labels, writer, milestone }) => {
+  return (
+    <$List>
+      <$LeftElements>
+        <$UpElements>
+          <Icon name="checkBoxInitial" />
+          <$IssueTitle>
+            <Icon name="alertCircle" fill="#007AFF" />
+            <span>{issueTitle}</span>
+            {labels.map(({ labelId, textColor, backgroundColor, labelName }) => {
+              return (
+                <Label
+                  key={labelId}
+                  height={24}
+                  textColor={textColor}
+                  backgroundColor={backgroundColor}
+                  name={labelName}
+                />
+              );
+            })}
+            <Label />
+          </$IssueTitle>
+        </$UpElements>
+        <$IssueInfo>
+          <span>{`#${issueId}`}</span>
+          <span>{`이 이슈가 1분전, ${writer.name}님에 의해 작성되었습니다.`}</span>
+          {milestone && <Icon name="milestone" fill="#6E7191" />}
+          <span>{milestone}</span>
+        </$IssueInfo>
+      </$LeftElements>
+
+      <$Assignee />
+    </$List>
+  );
 };
 
-List.propTypes = {};
+List.propTypes = {
+  issueId: PropTypes.number.isRequired,
+  issueTitle: PropTypes.string.isRequired,
+  labels: PropTypes.arrayOf(PropTypes.object),
+  writer: PropTypes.object,
+  milestone: PropTypes.string,
+};
 
 export default List;
--- FE/src/components/IssueList/IssueListMain/List/style.js
+++ FE/src/components/IssueList/IssueListMain/List/style.js
@@ -3,6 +3,61 @@ import styled from 'styled-components';
 const $List = styled.li`
   width: 100%;
   height: 100px;
+  // TODO: Remove
+  background-color: #fefefe;
 `;
 
-export { $List };
+const $LeftElements = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+`;
+
+const $UpElements = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  height: 50%;
+  & > svg {
+    margin-left: 32px;
+    margin-right: 32px;
+  }
+`;
+const $IssueTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fontSize.L.fontSize};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  & > svg {
+    margin-right: 8px;
+  }
+  & > span {
+    margin-right: 8px;
+  }
+`;
+
+const $IssueInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  text-align: center;
+  margin-left: 80px;
+  height: 50%;
+  font-size: ${({ theme }) => theme.fontSize.M.fontSize};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  color: ${({ theme }) => theme.colors.neutral.text.weak};
+  & > span {
+    margin-right: 16px;
+  }
+  & > svg {
+    margin-right: 8px;
+  }
+`;
+
+const $Assignee = styled.img`
+  margin-right: 54px;
+`;
+
+export { $List, $LeftElements, $UpElements, $IssueTitle, $IssueInfo, $Assignee };
