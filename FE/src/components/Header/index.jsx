14f52a15651090b550451cc4e181e7a07122c207
JangHoonSeok <orirori512@naver.com>
2023-05-18 16:41:51 +0900

    🐛 fix: Icon컴포넌트 prop type 변경

--- FE/src/components/Header/index.jsx
+++ FE/src/components/Header/index.jsx
@@ -8,15 +8,14 @@ import { $Header, $RightElements } from './style';
 const Header = ({ userImgSrc, userName }) => {
   const [themeMode, toggleTheme] = useTheme();
 
-  console.log(themeMode);
   return (
     <$Header>
       <a href="/">
-        <Icon width="199" height="25" name="defaultLogo" />
+        <Icon width={199} height={25} name="defaultLogo" />
       </a>
       <$RightElements>
         <button type="button" onClick={toggleTheme}>
-          <Icon width="32" height="32" name={themeMode === 'light' ? 'lightMode' : 'darkMode'} />
+          <Icon width={32} height={32} name={themeMode === 'light' ? 'lightMode' : 'darkMode'} />
         </button>
         <img src={userImgSrc} alt={`@${userName}`} />
       </$RightElements>
