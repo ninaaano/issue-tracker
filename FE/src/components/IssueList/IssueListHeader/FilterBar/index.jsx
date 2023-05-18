bff719657541ef8af60c79b9a4c6ec6bdc07f336
JangHoonSeok <orirori512@naver.com>
2023-05-18 16:40:54 +0900

    ✨ feat: 필터바 드롭다운 메뉴 초기 설정

--- FE/src/components/IssueList/IssueListHeader/FilterBar/index.jsx
+++ FE/src/components/IssueList/IssueListHeader/FilterBar/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Icon from '../../../common/Icon';
+import DropDown from '../../../common/DropDown';
 import { $FilterBar, $FilterButton, $FilterInput, $FilterInputWrapper } from './style';
+
+const FILTER = {
+  TYPE: 'issue',
+  MENUS: [
+    { text: '열린 이슈' },
+    { text: '내가 작성한 이슈' },
+    { text: '나에게 할당된 이슈' },
+    { text: '내가 댓글을 남긴 이슈' },
+    { text: '닫힌 이슈' },
+  ],
+};
 
 const FilterBar = () => {
   // 상태 : Initial, Active, Empty, Typing, Typed
-  const buttonClickHandler = () => {};
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const buttonClickHandler = () => {
+    setIsDropdownOpen((previous) => !previous);
+  };
 
   return (
     <$FilterBar>
@@ -14,6 +30,7 @@ const FilterBar = () => {
         필터
         <Icon name="chevronDown" fill="#6E7191" />
       </$FilterButton>
+      {isDropdownOpen && <DropDown type={FILTER.TYPE} menus={FILTER.MENUS} />}
       <$FilterInputWrapper>
         <Icon name="search" fill="#6E7191" />
         <$FilterInput type="text" placeholder="is:issue is:open" />
--- FE/src/components/common/DropDown/index.jsx
+++ FE/src/components/common/DropDown/index.jsx
@@ -4,15 +4,22 @@ import PropTypes from 'prop-types';
 import { $DropDownHeader, $DropDownMenus, $DropDownWrapper } from './style';
 import DropDownMenu from './DropDownMenu';
 
-const DropDown = ({ modalName, menus }) => {
+const filterName = {
+  issue: '이슈',
+  milestone: '마일스톤',
+  label: '레이블',
+  writer: '작성자',
+  assignee: '담당자',
+};
+
+const DropDown = ({ type, menus }) => {
   return (
     <$DropDownWrapper>
-      <$DropDownHeader>{modalName}</$DropDownHeader>
+      <$DropDownHeader>{`${filterName[type]} 필터`}</$DropDownHeader>
       <$DropDownMenus>
         {menus.map(({ id, imgSrc, text, isChecked }) => (
           /*
             menu : {
-                id: map으로 돌리기 위한 id 설정.
                 imgSrc: 이미지 주소 or 어떻게 할지 생각.
                 text: 메뉴 텍스트 ex) 내가 작성한 이슈
                 isChecked: 체크된 상태
@@ -26,7 +33,7 @@ const DropDown = ({ modalName, menus }) => {
 };
 
 DropDown.propTypes = {
-  modalName: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   menus: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
