f0b20e210252d86aba15342a9cc63145f96f1768
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-05-22 02:01:24 +0900

    Remove: schema.sql 삭제

--- BE/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists issue;
-
-create table issue (
-    issue_id int auto_increment not null,
-    issue_writer int not null,
-    issue_title varchar(45) not null,
-    issue_contents varchar(500),
-    primary key (issue_id)
-);
