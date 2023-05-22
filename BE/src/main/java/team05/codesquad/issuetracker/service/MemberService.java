66016b3a9db7025523a90b0a27050b2117d59428
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-05-22 12:50:46 +0900

    Refactor: MemberService.java 코드 수정
    @Transactional 어노테이션 추가

--- BE/src/main/java/team05/codesquad/issuetracker/service/MemberService.java
+++ BE/src/main/java/team05/codesquad/issuetracker/service/MemberService.java
@@ -1,13 +1,13 @@
 package team05.codesquad.issuetracker.service;
 
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.transaction.annotation.Transactional;
 import team05.codesquad.issuetracker.domain.Member;
 
 import java.util.List;
 
 @Getter
-@Setter
+@Transactional
 public class MemberService {
     private String name;
     private List<Member> memberList;
