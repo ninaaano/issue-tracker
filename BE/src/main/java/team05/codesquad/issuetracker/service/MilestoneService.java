7a220ef0e9899b0d6532b823d524fb41c24f4cf1
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-05-22 12:49:19 +0900

    Refactor: MilestoneService.java 코드 수정
    기존 사용되던 DTO를 전면 수정 한 관계로 MilestoneService 내부의 createMilestone() 메서드 반환 값 및 로직 수정

--- BE/src/main/java/team05/codesquad/issuetracker/service/MilestoneService.java
+++ BE/src/main/java/team05/codesquad/issuetracker/service/MilestoneService.java
@@ -3,15 +3,18 @@ package team05.codesquad.issuetracker.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import team05.codesquad.issuetracker.domain.milestone.Milestone;
-import team05.codesquad.issuetracker.dto.milestone.NewMilestoneDto;
+import team05.codesquad.issuetracker.dto.request.MilestoneCreateRequest;
+import team05.codesquad.issuetracker.dto.response.MilestoneCreateResponse;
 import team05.codesquad.issuetracker.repository.MilestoneRepository;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 
-@Service
 @Slf4j
+@Service
+@Transactional
 public class MilestoneService {
 
     private static final boolean STATUS_DEFAULT = true;
@@ -23,14 +26,14 @@ public class MilestoneService {
         this.milestoneRepository = milestoneRepository;
     }
 
-    public Milestone createMilestone(NewMilestoneDto newMilestoneDto) {
-        log.info(">>> MilestoneService createMilestone");
-        String title = newMilestoneDto.getTitle();
-        String description = newMilestoneDto.getDescription();
-        LocalDate deadLine = newMilestoneDto.getDeadLine();
-        Milestone milestone = new Milestone(title, description, deadLine, STATUS_DEFAULT, new ArrayList<>());
-        return milestoneRepository.save(milestone);
+    public MilestoneCreateResponse createMilestone(@RequestBody MilestoneCreateRequest request) {
+        Milestone milestone = new Milestone(request.getTitle(), request.getDescription(), request.getDeadLine(), STATUS_DEFAULT, new ArrayList<>());
+        Milestone savedMilestone = milestoneRepository.save(milestone);
+        return new MilestoneCreateResponse(savedMilestone.getId());
     }
 
-
+//    public List<Milestone> allMilestones() {
+//        log.info(">>> MilestoneService allMilestones");
+//        List<Milestone> allMilestones = milestoneRepository.findAll();
+//    }
 }
