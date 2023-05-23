d907f1d40ff0b90747e7fdc36bebb734cac94580
JangHoonSeok <orirori512@naver.com>
2023-05-23 17:07:12 +0900

    ✨ feat: 필터 reducer 생성

--- FE/src/context/filterReducer.js
+++ FE/src/context/filterReducer.js
@@ -1,1 +1,24 @@
-export const filterReducer = (state, action) => {};
+export const FILTER_ACTION_TYPES = {
+  CLICK_MENU: 'CLICK_MENU',
+};
+
+export const filterReducer = (state, action) => {
+  switch (action.type) {
+    case FILTER_ACTION_TYPES.CLICK_MENU: {
+      const { filterType, id } = action.payload;
+      const newFilter = { ...state[filterType] };
+
+      Object.keys(newFilter).forEach((key) => {
+        newFilter[key] = false;
+      });
+
+      newFilter[id] = !state[filterType][id];
+      const newState = { ...state, [filterType]: newFilter };
+
+      return newState;
+    }
+
+    default:
+      return state;
+  }
+};
