a7512ce971d6fb089fd51957ab2a0a0338473a69
YouSooA <kinsk2839@naver.com>
2023-05-23 22:25:09 +0900

    feat: type에 따라 Input 컴포넌트 스타일
    - direction: row, column에 따라 label 위치 변경
    - heightType: S, M에 따라 height 변경
    - 추후에 helpText, iconName은 prop에 추가 예정

--- FE/src/components/common/Input/index.jsx
+++ FE/src/components/common/Input/index.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ id, labelText, placeholderText, helpText }) => {
+import { $Input } from './style';
+
+// ! 추후에 prop에 helpText, iconName 추가
+const Input = ({
+  id,
+  direction = 'row',
+  labelText = '',
+  placeholderText = '',
+  disabled = false,
+  heightType = 'S',
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef();
+
+  const focusHandler = () => setIsFocused(true);
+  const blurHandler = () => setIsFocused(false);
+
+  const hasValue = inputRef.current.value.trim().length > 0;
+
   return (
     <React.Fragment>
-      <label htmlFor={id}>{labelText}</label>
-      <input type="text" id={id} name={id} placeholder={placeholderText} />
-      {helpText && <div>{helpText}</div>}
+      <$Input
+        direction={direction}
+        heightType={heightType}
+        labelText={labelText}
+        hasValue={hasValue}
+        isFocused={isFocused}
+        disabled={disabled}
+      >
+        <label htmlFor={id}>{labelText}</label>
+        <input
+          type="text"
+          id={id}
+          name={id}
+          placeholder={placeholderText}
+          ref={inputRef}
+          disabled={disabled}
+          onFocus={focusHandler}
+          onBlur={blurHandler}
+        />
+      </$Input>
+      {/* {helpText && <div>{helpText}</div>} */}
     </React.Fragment>
   );
 };
 
 Input.propTypes = {
   id: PropTypes.string.isRequired,
-  labelText: PropTypes.string.isRequired,
+  direction: PropTypes.oneOf(['row', 'column']),
+  labelText: PropTypes.string,
   placeholderText: PropTypes.string,
-  helpText: PropTypes.string,
+  disabled: PropTypes.bool,
+  heightType: PropTypes.oneOf(['S', 'M']),
+  // helpText: PropTypes.string,
 };
 
 export default Input;
---
+++ FE/src/components/common/Input/style.js
@@ -0,0 +1,66 @@
+import styled, { css } from 'styled-components';
+
+const label = css`
+  & > label {
+    width: ${({ labelText }) => (labelText ? '72px' : '0px')};
+
+    color: ${({ theme }) => theme.colors.neutral.text.default};
+    font-size: ${({ theme }) => theme.fontSize.S.fontSize};
+    line-height: ${({ theme }) => theme.fontSize.S.lineHeight};
+    font-weight: ${({ theme }) => theme.fontWeight.regular};
+  }
+`;
+
+const textInput = css`
+  & > input {
+    width: 100%;
+
+    color: ${({ hasValue, theme }) => theme.colors.neutral.text[hasValue ? 'strong' : 'weak']};
+    font-size: ${({ theme }) => theme.fontSize.M.fontSize};
+    line-height: ${({ theme }) => theme.fontSize.M.lineHeight};
+    font-weight: 400;
+  }
+  & > input:focus {
+    color: ${({ theme }) => theme.colors.neutral.text.strong};
+  }
+`;
+
+const row = css`
+  flex-direction: row;
+  align-items: center;
+  gap: ${({ labelText }) => (labelText ? '8px' : '0px')};
+`;
+
+const column = css`
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const flexDirection = css`
+  ${({ direction }) => {
+    if (direction === 'row') return row;
+    if (direction === 'column') return column;
+
+    return '';
+  }}
+`;
+
+const $Input = styled.div`
+  display: flex;
+  ${flexDirection}
+
+  height: ${({ heightType }) => (heightType === 'S' ? '40px' : '56px')};
+  padding: 0 24px;
+
+  border-radius: 14px;
+
+  background-color: ${({ isFocused, theme }) => {
+    return theme.colors.neutral.background[isFocused ? 'strong' : 'bold'];
+  }};
+  opacity: ${({ disabled }) => (disabled ? 0.32 : 1)};
+
+  ${label}
+  ${textInput};
+`;
+
+export { $Input };
