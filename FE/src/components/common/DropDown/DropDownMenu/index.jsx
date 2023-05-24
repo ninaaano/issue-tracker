67428eac17ed47119507e52c57004772805cffcc
JangHoonSeok <orirori512@naver.com>
2023-05-25 00:48:15 +0900

    ✨ feat: 드롭다운 컴포넌트 사이드바에 지원되도록 변경

--- FE/src/components/common/DropDown/DropDownMenu/index.jsx
+++ FE/src/components/common/DropDown/DropDownMenu/index.jsx
@@ -7,19 +7,34 @@ import { FILTER_ACTION_TYPES } from '../../../../context/filterReducer';
 import Icon from '../../Icon';
 import { $MenuWrapper, $MenuLeftWrapper, $MenuImg, $MenuText, $LabelColor } from './style';
 
-const DropDownMenu = ({ menuId, menuType, menuImg = null, menuText, backgroundColor }) => {
-  const filterDispatch = useContext(FilterDispatchContext);
+const DropDownMenu = ({ menuId, menuType, menuImg = null, menuText, backgroundColor, dropDownType }) => {
+  if (dropDownType !== 'sideBar') {
+    const filterDispatch = useContext(FilterDispatchContext);
 
-  const filterState = useContext(FilterStateContext);
-  const checkedOption = filterState[menuType];
+    const filterState = useContext(FilterStateContext);
+    const checkedOption = filterState[menuType];
 
-  const isChecked = checkedOption === menuId;
-  const menuClickHandler = () => {
-    filterDispatch({ type: FILTER_ACTION_TYPES.CLICK_MENU, payload: { filterType: menuType, id: menuId } });
-  };
+    const isChecked = checkedOption === menuId;
+    const menuClickHandler = () => {
+      filterDispatch({ type: FILTER_ACTION_TYPES.CLICK_MENU, payload: { filterType: menuType, id: menuId } });
+    };
+
+    return (
+      <$MenuWrapper type="button" onClick={dropDownType !== 'sideBar' ? menuClickHandler : () => {}}>
+        <$MenuLeftWrapper>
+          {backgroundColor && <$LabelColor backgroundColor={backgroundColor} />}
+          {menuImg !== null && <$MenuImg src={menuImg} />}
+          <$MenuText $isChecked={isChecked}>{menuText}</$MenuText>
+        </$MenuLeftWrapper>
+        <Icon name={isChecked ? 'checkOnCircle' : 'checkOffCircle'} />
+      </$MenuWrapper>
+    );
+  }
+
+  const isChecked = false;
 
   return (
-    <$MenuWrapper type="button" onClick={menuClickHandler}>
+    <$MenuWrapper type="button" onClick={() => {}}>
       <$MenuLeftWrapper>
         {backgroundColor && <$LabelColor backgroundColor={backgroundColor} />}
         {menuImg !== null && <$MenuImg src={menuImg} />}
@@ -36,6 +51,7 @@ DropDownMenu.propTypes = {
   menuImg: PropTypes.string,
   menuText: PropTypes.string.isRequired,
   backgroundColor: PropTypes.string,
+  dropDownType: PropTypes.string,
 };
 
 export default DropDownMenu;
