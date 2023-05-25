9ed7829d43f3611093c4b323b08e5ed606b1318e
YouSooA <kinsk2839@naver.com>
2023-05-26 00:33:32 +0900

    feat: NewIssue에서 데이터 fetch 및 submitHandler 생성

--- FE/src/components/NewIssueForm/index.jsx
+++ FE/src/components/NewIssueForm/index.jsx
@@ -1,38 +1,40 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-// import { USERS, LABELS, MILESTONES } from '../../constants/api';
-
-// import useFetch from '../../hooks/useFetch';
-
 import Button from '../common/Button';
 import Icon from '../common/Icon';
 import TextInput from '../common/TextInput';
 import TextArea from '../common/TextArea';
+import SideBar from '../common/SideBar';
 import { $NewIssueForm, $NewIssueFormMain, $UserImg, $InputWrapper, $SubmitButtonWrapper } from './style';
 
-const NewIssueForm = ({ userImgSrc }) => {
+const NewIssueForm = ({ userImgSrc, userData, labelData, milestoneData }) => {
   const [title, setTitle] = useState('');
   const [comment, setComment] = useState('');
 
   const changeTitleHandler = ({ target }) => setTitle(target.value);
   const changeCommentHandler = ({ target }) => setComment(target.value);
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <$NewIssueForm>
+    <$NewIssueForm onSubmit={submitHandler}>
       <$NewIssueFormMain>
         <$UserImg src={userImgSrc} alt="myImg" />
         <$InputWrapper>
           <TextInput id="issueTitle" value={title} onChange={changeTitleHandler} labelText="제목" />
           <TextArea id="issueComment" value={comment} onChange={changeCommentHandler} size="L" />
         </$InputWrapper>
+        <SideBar assignees={userData} labels={labelData} milestones={milestoneData} />
       </$NewIssueFormMain>
       <$SubmitButtonWrapper>
         <Button type="ghost" size="S">
           <Icon name="xSquare" />
           <p>작성 취소</p>
         </Button>
-        <Button type="contained" size="L">
+        <Button type="contained" size="L" onClick={submitHandler} disabled={title.length === 0}>
           완료
         </Button>
       </$SubmitButtonWrapper>
@@ -42,6 +44,9 @@ const NewIssueForm = ({ userImgSrc }) => {
 
 NewIssueForm.propTypes = {
   userImgSrc: PropTypes.string,
+  userData: PropTypes.array.isRequired,
+  labelData: PropTypes.array.isRequired,
+  milestoneData: PropTypes.array.isRequired,
 };
 
 export default NewIssueForm;
