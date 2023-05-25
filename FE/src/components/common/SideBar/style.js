c7e798c8113917a219465980e120f88d651c6583
JangHoonSeok <orirori512@naver.com>
2023-05-25 22:57:57 +0900

    ✨ feat: 사이드바 height 수정

--- FE/src/components/common/SideBar/style.js
+++ FE/src/components/common/SideBar/style.js
@@ -4,6 +4,7 @@ const $SideBar = styled.section`
   display: flex;
   flex-direction: column;
   width: 288px;
+  height: fit-content;
   background-color: ${({ theme }) => theme.colors.neutral.background.strong};
   border: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
   border-radius: 16px;
