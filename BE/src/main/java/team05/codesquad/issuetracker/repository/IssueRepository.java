b0c6a8b0191c1d0c829f8b894c1f5f444cf30475
Life is a ninanino <tvxqinak@naver.com>
2023-05-25 19:56:26 +0900

    feat: 열림/닫힘 이슈 목록

--- BE/src/main/java/team05/codesquad/issuetracker/repository/IssueRepository.java
+++ BE/src/main/java/team05/codesquad/issuetracker/repository/IssueRepository.java
@@ -1,9 +1,17 @@
 package team05.codesquad.issuetracker.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import team05.codesquad.issuetracker.domain.issue.Issue;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IssueRepository extends CrudRepository<Issue, Long> {
+
+    @Query("SELECT * from Issue where status = :isOpened")
+    public List<Issue> findByIsOpened(boolean isOpened);
+
 }
