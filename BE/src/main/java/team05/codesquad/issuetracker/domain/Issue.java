316147c1ead59dd864767c92a1b336ba46fe7cf5
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-05-25 11:17:53 +0900

    Refactor: Issue.java 코드 수정
    필요한 어노테이션 작성 및 class import문 작성

--- BE/src/main/java/team05/codesquad/issuetracker/domain/Issue.java
+++ BE/src/main/java/team05/codesquad/issuetracker/domain/Issue.java
@@ -2,17 +2,40 @@ package team05.codesquad.issuetracker.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import team05.codesquad.issuetracker.domain.milestone.Milestone;
 
-@Data
+import java.time.LocalDate;
+
+@Getter
 @Builder
+@Table("issue")
 public class Issue {
 
     @Id
+    @Column("id")
     private Long issueId;
+
+    @Column("writer_id")
     private int issueWriter;
+
+    @Column("milestone_id")
+    private AggregateReference<Milestone, Long> milestoneId;
+
+    @Column("title")
     private String issueTitle;
+
+    @Column("contents")
     private String issueContents;
+
+    @Column("created_at")
+    private LocalDate createdAt;
+
+    @Column("status")
     private boolean status;
 
 }
