408ac60e2ca12ea9c1973f2c9ae1609377eb2e7f
YouSooA <kinsk2839@naver.com>
2023-05-25 14:49:12 +0900

    feat: Input에서 labelText만 있는 경우 style 적용

--- FE/src/components/common/Input/index.jsx
+++ FE/src/components/common/Input/index.jsx
@@ -1,17 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import { $Input, $Label, $TextInput } from './style';
 
 // ! 추후에 prop에 helpText, iconName 추가
-const Input = ({
-  id,
-  direction = 'row',
-  labelText = '',
-  placeholderText = '',
-  disabled = false,
-  size = 'S',
-}) => {
+// labelText, placeholderText => direction = row
+// labelText => direction = column
+// placeholderText => direction = row
+const Input = ({ id, labelText = '', placeholderText = '', disabled = false }) => {
   const [isFocused, setIsFocused] = useState(false);
   const inputRef = useRef(null);
 
@@ -20,19 +16,22 @@ const Input = ({
 
   const hasValue = inputRef.current?.value.trim().length > 0;
 
+  const styleType = useMemo(() => {
+    if (labelText && placeholderText) return 'both';
+    if (labelText && !placeholderText) return 'onlyLabel';
+    if (!labelText && placeholderText) return 'onlyPlaceholder';
+
+    return null;
+  }, [labelText, placeholderText]);
+
   return (
     <React.Fragment>
-      <$Input
-        direction={direction}
-        size={size}
-        labelText={labelText}
-        hasValue={hasValue}
-        isFocused={isFocused}
-        disabled={disabled}
-      >
-        <$Label htmlFor={id} labelText={labelText}>
-          {labelText}
-        </$Label>
+      <$Input styleType={styleType} isFocused={isFocused} disabled={disabled}>
+        {labelText && (
+          <$Label htmlFor={id} styleType={styleType} hasValue={hasValue} isFocused={isFocused}>
+            {labelText}
+          </$Label>
+        )}
         <$TextInput
           type="text"
           id={id}
@@ -40,10 +39,10 @@ const Input = ({
           placeholder={placeholderText}
           ref={inputRef}
           hasValue={hasValue}
-          disabled={disabled}
           isFocused={isFocused}
           onFocus={focusHandler}
           onBlur={blurHandler}
+          disabled={disabled}
         />
       </$Input>
       {/* {helpText && <div>{helpText}</div>} */}
@@ -53,11 +52,11 @@ const Input = ({
 
 Input.propTypes = {
   id: PropTypes.string.isRequired,
-  direction: PropTypes.oneOf(['row', 'column']),
+  // direction: PropTypes.oneOf(['row', 'column']),
   labelText: PropTypes.string,
   placeholderText: PropTypes.string,
   disabled: PropTypes.bool,
-  size: PropTypes.oneOf(['S', 'M']),
+  // size: PropTypes.oneOf(['S', 'M']),
   // helpText: PropTypes.string,
 };
 
