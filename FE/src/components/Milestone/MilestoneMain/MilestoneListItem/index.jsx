b972005b267f42cafbdcf9266263cae001438083
JangHoonSeok <orirori512@naver.com>
2023-05-27 21:28:04 +0900

    ✨ feat: 마일스톤 메인뷰 아이템 파일 생성

---
+++ FE/src/components/Milestone/MilestoneMain/MilestoneListItem/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MilestoneListItem = (props) => {
+  return <div>MilestoneListItem</div>;
+};
+
+MilestoneListItem.propTypes = {};
+
+export default MilestoneListItem;
