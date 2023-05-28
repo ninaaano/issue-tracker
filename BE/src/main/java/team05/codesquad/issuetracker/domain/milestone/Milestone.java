f4a079015e04608351e7f3fc046adb594b26b294
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-05-28 13:28:59 +0900

    Refactor: 린트 수정
    가독성을 위한 어노테이션 위치 수정

--- BE/src/main/java/team05/codesquad/issuetracker/domain/milestone/Milestone.java
+++ BE/src/main/java/team05/codesquad/issuetracker/domain/milestone/Milestone.java
@@ -15,7 +15,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Builder @Getter
+@Builder
+@Getter
 @AllArgsConstructor
 @Table("milestone")
 public class Milestone {
