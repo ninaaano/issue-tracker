7b4f90f38816606cd0c17ea2fd643a06a397ca7e
JangHoonSeok <orirori512@naver.com>
2023-05-30 00:37:34 +0900

    ✨ feat: 배포 위해 worker 조건문 해제

--- FE/src/index.jsx
+++ FE/src/index.jsx
@@ -4,9 +4,9 @@ import { StrictMode, React } from 'react';
 import App from './App';
 import { worker } from './mocks/worker';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+worker.start();
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
