5a7b10317284e13680cccede374d85ed725da0e3
Life is a ninanino <tvxqinak@naver.com>
2023-05-29 18:11:13 +0900

    refactor: sql data 수정

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,21 +1,50 @@
-INSERT INTO milestone VALUES (1,'miletest',null,null,true);
+-- milestone 테이블 더미 데이터 추가
 
-INSERT INTO member VALUES (1,'test','1234','test');
-INSERT INTO member VALUES (2,'test2','1234','test');
+INSERT INTO `milestone` (`TITLE`, `DESCRIPTION`, `DEADLINE`)
+VALUES ('마일스톤 1번 제목', '마일스톤 1번 내용', '1997-07-10');
+INSERT INTO `milestone` (`TITLE`, `DESCRIPTION`, `DEADLINE`)
+VALUES ('마일스톤 2번 제목', '마일스톤 2번 내용', '2023-05-10');
+INSERT INTO `milestone` (`TITLE`, `DESCRIPTION`, `DEADLINE`)
+VALUES ('마일스톤 3번 제목', '마일스톤 3번 내용', '2023-05-15');
 
-INSERT INTO issue VALUES (1,'test',null,1,true,1,'2023-05-25');
-INSERT INTO issue VALUES (2,'test2',null,1,true,1,'2023-05-25');
-INSERT INTO issue VALUES (3,'test3',null,1,false,null,'2023-05-25');
+-- member 테이블 더미 데이터 추가
 
-INSERT INTO label VALUES (1,'1','test1','test','test');
-INSERT INTO label VALUES (2,'2','test2','test','test');
-INSERT INTO label VALUES (3,'3','test3','test','test');
-INSERT INTO label VALUES (4,'4','test4','test','test');
+INSERT INTO `member` (`NAME`, `PASSWORD`)
+VALUES ('고건호', '1q2w3e4r!');
+INSERT INTO `member` (`NAME`, `PASSWORD`, `PROFILE_URL`)
+VALUES ('노민정', '1234', 'https://team05.s3.amazonaws.com/ninanino');
 
-INSERT INTO label_attached_issues VALUES (1,1);
-INSERT INTO label_attached_issues VALUES (1,2);
-INSERT INTO label_attached_issues VALUES (1,3);
-INSERT INTO label_attached_issues VALUES (1,4);
+-- issue 테이블 더미 데이터 추가
 
-INSERT INTO label_attached_issues VALUES (2,3);
-INSERT INTO label_attached_issues VALUES (2,4);
+INSERT INTO `issue` (`TITLE`, `CONTENTS`, `WRITER_ID`, `CREATED_AT`)
+VALUES ('이슈 1번 제목', '이슈 1번 내용', 1, '2023-04-25');
+INSERT INTO `issue` (`TITLE`, `CONTENTS`, `WRITER_ID`, `CREATED_AT`)
+VALUES ('이슈 2번 제목', '이슈 2번 내용', 2, '2023-04-27');
+INSERT INTO `issue` (`TITLE`, `CONTENTS`, `WRITER_ID`, `CREATED_AT`)
+VALUES ('이슈 3번 제목', '이슈 3번 내용', 2, '2023-05-05');
+INSERT INTO `issue` (`TITLE`, `CONTENTS`, `WRITER_ID`, `IS_OPENED`, `MILESTONE_ID`, `CREATED_AT`)
+VALUES ('이슈 4번 제목', '이슈 4번 내용', 1, false, 2, '2023-05-07');
+
+-- label 테이블 더미 데이터 추가
+
+INSERT INTO `label` (`TITLE`, `DESCRIPTION`, `BACKGROUND_COLOR`, `FONT_COLOR`)
+VALUES ('레이블 1번 제목', '레이블 1번 설명', '#00FF00', 'LIGHT');
+INSERT INTO `label` (`TITLE`, `DESCRIPTION`, `BACKGROUND_COLOR`, `FONT_COLOR`)
+VALUES ('레이블 2번 제목', '레이블 2번 설명', '##FFFF00', 'DARK');
+INSERT INTO `label` (`TITLE`, `DESCRIPTION`, `BACKGROUND_COLOR`, `FONT_COLOR`)
+VALUES ('레이블 3번 제목', '레이블 3번 설명', '#0000FF', 'LIGHT');
+INSERT INTO `label` (`TITLE`, `DESCRIPTION`, `BACKGROUND_COLOR`, `FONT_COLOR`)
+VALUES ('레이블 4번 제목', '레이블 4번 설명', '#808080', 'DARK');
+
+-- label_attached_issues 더미 데이터 추가
+
+INSERT INTO `label_attached_issues`
+VALUES (1, 1);
+INSERT INTO `label_attached_issues`
+VALUES (1, 2);
+INSERT INTO `label_attached_issues`
+VALUES (2, 2);
+INSERT INTO `label_attached_issues`
+VALUES (2, 3);
+INSERT INTO `label_attached_issues`
+VALUES (3, 4);
