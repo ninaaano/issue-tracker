0d500dce68d9af11139b75326cb4b1eb3da09748
YouSooA <kinsk2839@naver.com>
2023-05-31 02:40:25 +0900

    refactor: IssueDetailMain 컴포넌트 리팩토링
    - IssueDetail에 있는 로직 IssueDetailMain으로 옮김

--- FE/src/components/IssueDetail/IssueDetailMain/index.jsx
+++ FE/src/components/IssueDetail/IssueDetailMain/index.jsx
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+import Icon from '../../common/Icon';
+import Button from '../../common/Button';
+import SideBar from '../../common/SideBar';
+import TextArea from '../../common/TextArea';
 import Comments from './Comments';
-import { $IssueDetailMain } from './style';
+import { $IssueDetailMain, $IssueCommentArea, $IssueDetailMainLayout } from './style';
 
-const IssueDetailMain = ({ issue }) => {
+const IssueDetailMain = ({ detailIssue, user: userData, label: labelData, milestone: milestoneData }) => {
+  const [selectedItems, setSelectedItems] = useState({
+    assignee: detailIssue.assignee[0].userId,
+    milestone: detailIssue.milestone.milestoneId,
+    label: detailIssue.label[0].labelId,
+  });
+  const [comment, setComment] = useState('');
+  const [files, setFiles] = useState([]);
+
+  const commentEditHandler = ({ target }) => {
+    setComment(target.value);
+  };
+  const filesUploadHandler = ({ target }) => {
+    setFiles([...target.files]);
+  };
+
+  const changeAssigneeHandler = (userId) => setSelectedItems({ ...selectedItems, assignee: userId });
+  const changeLabelHandler = (labelId) => setSelectedItems({ ...selectedItems, label: labelId });
+  const changeMilestoneHandler = (milestoneId) => {
+    setSelectedItems({ ...selectedItems, milestone: milestoneId });
+  };
+
+  console.log(selectedItems);
+
   return (
-    <$IssueDetailMain>
-      <Comments issue={issue} />
-    </$IssueDetailMain>
+    <$IssueDetailMainLayout>
+      <$IssueCommentArea>
+        <$IssueDetailMain>
+          <Comments issue={detailIssue} />
+        </$IssueDetailMain>
+        <TextArea
+          id="comment"
+          value={comment}
+          onChange={commentEditHandler}
+          size="S"
+          files={files}
+          filesUploadHandler={filesUploadHandler}
+        />
+        <Button type="contained" size="S">
+          <Icon name="plus" />
+          코멘트 작성
+        </Button>
+      </$IssueCommentArea>
+      <SideBar
+        assignees={userData}
+        labels={labelData}
+        milestones={milestoneData}
+        selectedItems={selectedItems}
+        changeAssigneeHandler={changeAssigneeHandler}
+        changeLabelHandler={changeLabelHandler}
+        changeMilestoneHandler={changeMilestoneHandler}
+      />
+    </$IssueDetailMainLayout>
   );
 };
 
 IssueDetailMain.propTypes = {
-  issue: PropTypes.object.isRequired,
+  detailIssue: PropTypes.object.isRequired,
+  user: PropTypes.array.isRequired,
+  label: PropTypes.array.isRequired,
+  milestone: PropTypes.array.isRequired,
 };
 
 export default IssueDetailMain;
--- FE/src/components/IssueDetail/IssueDetailMain/style.js
+++ FE/src/components/IssueDetail/IssueDetailMain/style.js
@@ -6,4 +6,22 @@ const $IssueDetailMain = styled.section`
   width: 100%;
 `;
 
-export { $IssueDetailMain };
+const $IssueCommentArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  position: relative;
+  width: 958px;
+
+  & > button {
+    margin-top: 24px;
+  }
+`;
+
+const $IssueDetailMainLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export { $IssueDetailMain, $IssueCommentArea, $IssueDetailMainLayout };
