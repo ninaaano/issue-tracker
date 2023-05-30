e0f4aa071f89d0be3400e354da540c1c9d28d832
YouSooA <kinsk2839@naver.com>
2023-05-30 20:55:57 +0900

    feat: SideBarMenu의 prop에 changeHandler 추가

--- FE/src/components/common/SideBar/SideBarMenu/index.jsx
+++ FE/src/components/common/SideBar/SideBarMenu/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { FILTER_TYPE, FILTER_NAME } from '../../../../constants/dropdownMenu';
@@ -6,7 +6,7 @@ import DropDown from '../../DropDown';
 
 import { $SideBarMenu, $SelectedItem } from './style';
 
-const SideBarMenu = ({ type, menus, selectedItemId }) => {
+const SideBarMenu = ({ type, menus, selectedItemId, changeHandler }) => {
   const [selectedItem, setSelectedItem] = useState({
     type,
     id: selectedItemId,
@@ -39,6 +39,10 @@ const SideBarMenu = ({ type, menus, selectedItemId }) => {
   };
   const selectItemInfo = findSelectItemInfo();
 
+  useEffect(() => {
+    changeHandler(selectedItem.id);
+  }, [selectedItem.id]);
+
   return (
     <$SideBarMenu isSelected={isSelectedItem}>
       <DropDown
@@ -60,6 +64,7 @@ SideBarMenu.propTypes = {
   type: PropTypes.string.isRequired,
   menus: PropTypes.array.isRequired,
   selectedItemId: PropTypes.any,
+  changeHandler: PropTypes.func,
 };
 
 export default SideBarMenu;
