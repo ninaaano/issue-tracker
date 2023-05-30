473c1d16a7b4614df8455304c061e8a0f95aa3ae
JangHoonSeok <orirori512@naver.com>
2023-05-30 16:12:26 +0900

    ✨ feat: 랜덤 color 16진수로 return 하는 util 함수 생성

---
+++ FE/src/utils/generateRandomColor.js
@@ -0,0 +1,13 @@
+const createRandomNumberToHexCode = (number) => {
+  return Math.floor(Math.random() * number).toString(16);
+};
+
+const generateRandomColor = () => {
+  const R = createRandomNumberToHexCode(256);
+  const G = createRandomNumberToHexCode(256);
+  const B = createRandomNumberToHexCode(256);
+
+  return `#${R}${G}${B}`;
+};
+
+export default generateRandomColor;
