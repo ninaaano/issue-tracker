ac0445dd32fe117a02b19272c2caa34c12dc8109
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-05-30 11:55:13 +0900

    Feat: MilestoneDto.java 정적 팩토리 메서드 추가

--- BE/src/main/java/team05/codesquad/issuetracker/controller/milestonedto/MilestoneDto.java
+++ BE/src/main/java/team05/codesquad/issuetracker/controller/milestonedto/MilestoneDto.java
@@ -2,9 +2,11 @@ package team05.codesquad.issuetracker.controller.milestonedto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team05.codesquad.issuetracker.domain.issue.Issue;
 import team05.codesquad.issuetracker.domain.milestone.Milestone;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -17,19 +19,33 @@ public class MilestoneDto {
     private Long openedIssues;
     private Long closedIssues;
     private Boolean isOpened;
+    private List<Issue> issues;
 
-    private MilestoneDto(Milestone milestone) { // 정적 팩토리 메서드를 위한 생성자
+    private MilestoneDto(Milestone milestone, Long openedIssues, Long closedIssues) { // 정적 팩토리 메서드를 위한 생성자
         this.id = milestone.getId();
         this.title = milestone.getTitle();
         this.description = milestone.getDescription();
         this.deadLine = milestone.getDeadLine();
-        this.openedIssues = milestone.countOpenIssues();
-        this.closedIssues = milestone.countCloseIssues();
         this.isOpened = true;
+        this.openedIssues = openedIssues;
+        this.closedIssues = closedIssues;
     }
 
-    public static MilestoneDto of(Milestone milestone) { // 정적 팩토리 메서드
-        return new MilestoneDto(milestone);
+    private MilestoneDto(Milestone milestone, List<Issue> issues) {
+        this.id = milestone.getId();
+        this.title = milestone.getTitle();
+        this.description = milestone.getDescription();
+        this.deadLine = milestone.getDeadLine();
+        this.isOpened = true;
+        this.issues = issues;
+    }
+
+    public static MilestoneDto of(Milestone milestone, List<Issue> issues) {
+        return new MilestoneDto(milestone, issues);
+    }
+
+    public static MilestoneDto of(Milestone milestone, Long openedIssues, Long closedIssues) { // 정적 팩토리 메서드
+        return new MilestoneDto(milestone, openedIssues, closedIssues);
     }
 
 }
