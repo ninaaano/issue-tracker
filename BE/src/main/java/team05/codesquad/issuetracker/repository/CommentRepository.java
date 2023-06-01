865fa05b6795f8e67588dbb47284728ff4212c40
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-01 16:56:28 +0900

    Refactor: CommentRepository.java 커스텀 쿼리문 수정

--- BE/src/main/java/team05/codesquad/issuetracker/repository/CommentRepository.java
+++ BE/src/main/java/team05/codesquad/issuetracker/repository/CommentRepository.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 
-    @Query("SELECT `comment_id`,`writer_id`,`issue_id`,`contents`,`created_at` FROM `comment` WHERE `comment_id` = :commentId AND `issue_id` = :issueId")
+    @Query("SELECT `comment_id`, `writer_id`, `writer_name`, `issue_id`,`contents`,`created_at` FROM `comment` WHERE `comment_id` = :commentId AND `issue_id` = :issueId")
     Comment findByCommentIdAndIssueId(@Param("commentId") Long commentId, @Param("issueId") Long issueId);
 
     @Modifying
     @Query("DELETE FROM `comment` WHERE `comment`.`comment_id`=:commentId AND `comment`.`issue_id`=:issueId")
     int deleteByCommentIdAndIssueId(@Param("commentId") Long commentId, @Param("issueId") Long issueId);
 
-    @Query("SELECT `comment_id`, `writer_id`, `issue_id`, `contents`, `created_at` FROM `comment` WHERE `issue_id`=:issueId")
+    @Query("SELECT `comment_id`, `writer_id`, `writer_name`, `issue_id`, `contents`, `created_at` FROM `comment` WHERE `issue_id`=:issueId")
     List<Comment> findByIssueId(@Param("issueId") Long issueId);
 }
