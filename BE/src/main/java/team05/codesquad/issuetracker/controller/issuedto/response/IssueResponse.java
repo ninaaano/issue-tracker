d0c76ded84cc0451f58e064a8e06ca51fa9775b9
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-01 11:48:31 +0900

    Feat: IssueResponse.java 메서드 추가

--- BE/src/main/java/team05/codesquad/issuetracker/controller/issuedto/response/IssueResponse.java
+++ BE/src/main/java/team05/codesquad/issuetracker/controller/issuedto/response/IssueResponse.java
@@ -1,7 +1,7 @@
 package team05.codesquad.issuetracker.controller.issuedto.response;
 
 import lombok.*;
-import team05.codesquad.issuetracker.controller.commentdto.CommentDto;
+import lombok.extern.slf4j.Slf4j;
 import team05.codesquad.issuetracker.controller.commentdto.response.CommentDtoListResponse;
 import team05.codesquad.issuetracker.controller.labeldto.response.LabelResponse;
 import team05.codesquad.issuetracker.domain.comment.Comment;
@@ -10,6 +10,7 @@ import team05.codesquad.issuetracker.domain.issue.Issue;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -26,7 +27,28 @@ public class IssueResponse {
     private Long milestoneId;
     private boolean isopened;
 
+    public IssueResponse(Long id, String title, LocalDateTime createdAt, List<LabelResponse> from, Long milestoneId, Boolean isOpened) {
+        this.id = id;
+        this.title = title;
+        this.createdAt = createdAt;
+        this.labels = from;
+        this.milestoneId = milestoneId;
+        this.isopened = isOpened;
+    }
+
+    public static IssueResponse from(Issue issue) {
+        Long milestoneId = null;
+        if (issue.getMilestone() != null) {
+            milestoneId = issue.getMilestone().getId();
+        }
+        return new IssueResponse(issue.getId(), issue.getTitle(), issue.getCreatedAt(), LabelResponse.from(issue.getLabels()), milestoneId, issue.getIsOpened());
+    }
+
     public static IssueResponse from(Issue issue, List<Comment> commentList) {
+        if (commentList == null) {
+            log.info(">>>>>>>>>> commentList가 null일때");
+            return from(issue);
+        }
         Long milestoneId = null;
         if (issue.getMilestone() != null) {
             milestoneId = issue.getMilestone().getId();
