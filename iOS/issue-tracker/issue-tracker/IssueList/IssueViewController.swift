0f750503ffb57cda1364386137e778e6636f199e
wnsqhs <wnsqhs7804@gmail.com>
2023-06-02 11:50:47 +0900

    feat: 서치 바 구현
    - 검색 기능 구현
    - 서치 바 구현

--- iOS/issue-tracker/issue-tracker/IssueList/IssueViewController.swift
+++ iOS/issue-tracker/issue-tracker/IssueList/IssueViewController.swift
@@ -12,27 +12,31 @@ final class IssueViewController: UIViewController {
     private var currentSnapshot: NSDiffableDataSourceSnapshot<Section, Item>!
     private var itemHttpHandler = HTTPHandler<Item>()
     private var data: [Item] = []
-    private let issueListCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-    
+    let issueListCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let searchController = UISearchController(searchResultsController: nil)
+    private let issueFilterNavigation = UINavigationController(rootViewController: IssueFilterViewController())
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.issueListCollectionView.backgroundColor = ColorValue.gray100
         self.issueListCollectionView.delegate = self
+        self.searchController.searchResultsUpdater = self
         self.layoutIssueListCollectionView()
         self.configureDataSource()
+        self.configureNavigationBar()
         self.setupData()
     }
     
     private func setupData() {
-        DispatchQueue.main.async {
-            self.itemHttpHandler.fetchIssue { result in
-                switch result {
-                case.success(let issueData):
+        self.itemHttpHandler.fetchIssue { result in
+            switch result {
+            case.success(let issueData):
+                DispatchQueue.main.async {
                     self.data = issueData
-                    self.configureSnapshot()
-                default:
-                    break
+                    self.configureSearchBar()
                 }
+            default:
+                break
             }
         }
     }
@@ -48,15 +52,32 @@ final class IssueViewController: UIViewController {
             issueListCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = "이슈"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "선택", style: .plain, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "필터", style: .plain, target: self, action: #selector(tappedFilterButton))
+    }
+    
+    private func configureSearchBar() {
+        self.navigationItem.searchController = searchController
+        self.searchController.searchBar.placeholder = "검색"
+        self.searchController.isActive = true
+        self.searchController.searchBar.becomeFirstResponder()
+    }
+    
+    @objc private func tappedFilterButton() {
+        self.present(issueFilterNavigation, animated: true)
+    }
+    
+    @objc private func tappedIssueButton() {
+        let desiredOffset = CGPoint(x: 0, y: 0)
+        issueListCollectionView.setContentOffset(desiredOffset, animated: true)
+    }
 }
 
 extension IssueViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        let width = view.frame.width
-        let height: CGFloat = 48
-        return CGSize(width: width, height: height)
-    }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = view.frame.width
         let height: CGFloat = 148
@@ -71,46 +92,45 @@ extension IssueViewController: UICollectionViewDelegateFlowLayout {
 extension IssueViewController: DiffableDataSourceManager {
     typealias ItemIndetifierType = Item
     typealias CellType = IssueCardCell
-    typealias ReusableView = IssueListHeaderView
     
     func createListCellRegistration() -> UICollectionView.CellRegistration<IssueCardCell, Item> {
         return UICollectionView.CellRegistration<IssueCardCell, Item> { (cell, _, item) in
             cell.titleLabel.text = item.issueTitle
-            cell.explanationLabel.text = "설명설명설명설명설명설명설명"
-            cell.milestoneLabel.text = item.milestone.milestoneName
+            cell.explanationLabel.text = item.createdAt
+            cell.milestoneLabel.text = "마일스톤"
             cell.labelStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
             item.label.forEach { data in
-                let custom = CustomCapsuleLabel(name: data.labelName, textColor: data.textColor, backgroundColor: data.backgroundColor, font: FontStyle.label.font)
+                let custom = CustomCapsuleLabel(name: data.labelName, textColor: data.fontColor, backgroundColor: data.backgroundColor, font: FontStyle.label.font)
                 cell.labelStackView.addArrangedSubview(custom)
             }
-        }
-    }
-    
-    func createHeaderCellRegistration() -> UICollectionView.SupplementaryRegistration<IssueListHeaderView> {
-        return UICollectionView.SupplementaryRegistration<IssueListHeaderView>(elementKind: UICollectionView.elementKindSectionHeader) { (cell, _, indexPath) in
-            let section = self.currentSnapshot.sectionIdentifiers[indexPath.section]
-            cell.title.text = section.title
         }
     }
 
     private func configureDataSource() {
         let listCellRegistration = createListCellRegistration()
-        let headerCellRegistration = createHeaderCellRegistration()
         
         self.dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: self.issueListCollectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
             return collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: itemIdentifier)
         })
-
-        self.dataSource.supplementaryViewProvider = { (collectionView, _, indexPath) in
-            return collectionView.dequeueConfiguredReusableSupplementary(using: headerCellRegistration, for: indexPath)
-        }
     }
 
-    private func configureSnapshot() {
+    private func performQuery(with filter: String?) {
+        let filtered = filteredData(with: filter)
         self.currentSnapshot = NSDiffableDataSourceSnapshot<Section, Item>()
         self.currentSnapshot.appendSections([.issue])
-        self.currentSnapshot.appendItems(self.data, toSection: .issue)
+        self.currentSnapshot.appendItems(filtered, toSection: .issue)
         
         self.dataSource.apply(self.currentSnapshot)
+    }
+    
+    private func filteredData(with filter: String?) -> [Item] {
+        return self.data.filter { $0.contains(filter) }
+    }
+}
+
+extension IssueViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let text = searchController.searchBar.text
+        performQuery(with: text)
     }
 }
