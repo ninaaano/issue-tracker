2b8a5e5f1a8286f021742c6ba37ce84b27d1f4f1
Life is a ninanino <tvxqinak@naver.com>
2023-06-02 15:58:57 +0900

    feat: 글쓴이를 조회하기 위해 Member API 생성

--- BE/src/main/java/team05/codesquad/issuetracker/repository/MemberRepository.java
+++ BE/src/main/java/team05/codesquad/issuetracker/repository/MemberRepository.java
@@ -1,10 +1,20 @@
 package team05.codesquad.issuetracker.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import team05.codesquad.issuetracker.domain.label.Label;
 import team05.codesquad.issuetracker.domain.member.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends CrudRepository<Member,Long> {
-    Optional<Member> findById(Long id);
+
+    @Query("SELECT m.member_id FROM member m " +
+            "LEFT OUTER JOIN assignee a ON a.member_id = m.member_id " +
+            "WHERE a.issue_id = :issueId")
+    List<Member> findByIssueId(Long issueId);
+
+    List<Member> findAll();
+
 }
